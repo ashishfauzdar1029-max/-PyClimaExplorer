c062e50e7038221573a4bf9a8ae924f408b4ad4e
ashishfauzdar1029-max <ashishfauzdar1029@gmail.com>
2026-03-14 18:19:08 +0530

    Add climate data dashboard with temperature chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
+st.title("Climate Data Dashboard")
+
+data = {
+    "Year": [1995, 2000, 2005, 2010, 2015],
+    "Temperature": [24.1, 24.5, 24.9, 25.2, 25.8]
+}
+
+df = pd.DataFrame(data)
+
+st.write("Climate Temperature Data")
+st.dataframe(df)
+
+st.line_chart(df.set_index("Year"))
